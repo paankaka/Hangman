1dea8672b0de7fdf4f5223a774db2f9ab1df18fa
paankaka <79742205+paankaka@users.noreply.github.com>
2021-03-07 21:24:18 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+name = input("What is your name? ")
+time.sleep(0.8)
+print("Good Luck ! ", name)
+words = ['techno', 'india', 'khakkhu', 'programming', 
+		'python', 'university', 'gamer', 'TCS', 
+		'whiskey', 'fresher', 'NQT'] 
+word = random.choice(words)
+time.sleep(0.5)
+print("Guess the word")
+guesses = ''
+turns = 12
+while turns > 0:
+	failed = 0
+	for char in word: 
+		if char in guesses: 
+			print(char)
+		else: 
+			print("_")
+			failed += 1
+	if failed == 0:
+		print("You Win") 
+		print("The word is: ", word) 
+		break
+	guess = input("guess a character:")
+	guesses += guess 
+	if guess not in word:
+		turns -= 1
+		print("Wrong")
+		print("You have", + turns, 'more guesses')
+		if turns == 0:
+			print("12 chance diye kuch toh sharam kr lo behuda", word, "likha tha")
+
